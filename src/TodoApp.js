22d16b8668a161ac7d611ff4b9aa142b3e58de36
pdex <isupd2002@yahoo.com.tw>
2017-09-21 10:54:35 +0800

    TodoApp study

---
+++ src/TodoApp.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import TodoHeader from './TodoHeader'  // 引入TodoHeader组件
+import TodoMain from './TodoMain'  // 引入TodoMain组件
+import './TodoList.css';
+
+class TodoApp extends React.Component { // 定义组件，继承父类
+    //TodoApp是UI,TodoHeader是input,TodoMain是input後的顯示結果,TodoItem是每個item的內容與核選
+  constructor() {
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+  addTodo(item) { // 新增了添加todo事项的方法
+    this.state.todos.push(item)
+    this.setState({todos: this.state.todos});  //设置状态 不寫這行UI不會被觸發改變
+  }
+  render(){
+    return (
+      <div className="todo-wrapper todo-input">
+        {/* // todo-input引用之前寫的css. 将原内容写在组件中并引入进行渲染
+        // 把addTodo方法传递到TodoHeader组件中，bind(this)是为了把该React实例绑定到this上 */}
+        <TodoHeader addTodo={this.addTodo.bind(this)}/>
+        {/* // 把 state.todos 传入到TodoMain 中 */}
+        <TodoMain todos={this.state.todos}/>
+      </div>
+    )
+  }
+}
+export default TodoApp;
--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import App from './App';
 import Hello from './Hello';
 import TextAeraCounter from './TextAeraCounter';
 import TodoList from './TodoList';
+import TodoApp from './TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
 <div>
 <App /> 
-<Hello name="Bob"/>
+{/* <Hello name="Bob"/>
 <Hello name=""/>
-<TextAeraCounter />
-<TodoList />
-<TodoList />
+<TextAeraCounter /> */}
+{/* <TodoList /> */}
+<TodoApp />
 </div>
 
 )
